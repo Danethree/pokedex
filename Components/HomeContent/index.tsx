6f8fa0223f3dd214c3e2f61cf168836f2301cce4
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-04-26 14:59:20 -0300

    Header and Home created

---
+++ Components/HomeContent/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function HomeContent(){return(<>
+    <h1
+    className="flex align-center justify-center text-2xl mt-10"
+    id = "logo"
+    >
+        WebDex - Your Web Pokédex
+      
+    
+    </h1>
+
+   
+
+    <p className="text-base font-light leading-relaxed m-5 text-neutral-800 flex align-center justify-center text-justify" >
+    Welcome Pokémon trainer!
+You'll find  here all you need to know about
+    your Pokémon! To run click in Search button in menu
+    </p>
+    </>
+)}
