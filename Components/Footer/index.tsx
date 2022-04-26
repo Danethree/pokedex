cbcb7f5b04ac55c658e2b788403b837bf99b27bf
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-04-26 15:05:39 -0300

    Footer Component created

---
+++ Components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer(){return(
+    <>
+    
+    <div className = "flex items-center justify-center flex-wrap bg-red-800 p-6 ">
+        <h6 className = "text-white">
+            WebDex - Created by Daniela F. D'Oliveira
+        </h6>
+    </div>
+    
+    </>
+
+)}
